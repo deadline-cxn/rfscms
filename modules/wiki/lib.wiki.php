4059db2e1c64fcc2acd70bd9a372b4f4fcebda0b
sethcoder <defectiveseth@gmail.com>
2013-04-10 14:36:35 -0400

    wiki update

--- modules/wiki/lib.wiki.php
+++ modules/wiki/lib.wiki.php
@@ -132,6 +132,8 @@ function wikitext($text) {
 
                         $outtext.="<div class='wikishell'><BR>";
 							$xx=$ila2[1];
+							$xx=str_replace("\","&#92;" ,$xx);
+														
 							//$xx=str_replace("\n","\n &nbsp; ",$xx);
 							//$xx=nl2br( $xx );
                         $outtext.=$xx;
