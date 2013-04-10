2b07d45c420613db7790915a51331b62945a9e6c
sethcoder <defectiveseth@gmail.com>
2013-04-10 14:04:47 -0400

    lib.wiki.php update
    Former-commit-id: c8166c21562f29ca20edd99f6fbe18eef6ae1f81

--- modules/wiki/lib.wiki.php
+++ modules/wiki/lib.wiki.php
@@ -3,6 +3,8 @@
 include_once("include/lib.all.php");
 
 sc_access_method_add("wiki", "admin");
+sc_access_method_add("wiki", "editothers");
+sc_access_method_add("wiki", "deleteothers");
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///// MODULE WIKI
