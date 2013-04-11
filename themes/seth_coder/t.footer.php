8b6f680bd992f89fa5a76f1d777281db9a3e24a2
sethcoder <defectiveseth@gmail.com>
2013-04-10 22:05:47 -0400

    theme updates

--- themes/seth_coder/t.footer.php
+++ themes/seth_coder/t.footer.php
@@ -1,23 +1,13 @@
 <?
 
-include($GLOBALS['site_path']."/rfs/site_vars.php");
-
-
-
-
+include("$RFS_SITE_URL/include/lib.sitevars.php");
 
 
 tcc();
-
 trc();
-
 tc();
 
-
-
 d_echo("[tmpl_test.footer.php]");
-
-
 
 			if(empty($data->donated)){
 				echo "<table border=0 width=100% ><tr><td align=center>";
@@ -29,15 +19,10 @@ d_echo("[tmpl_test.footer.php]");
 
 				}
 				echo "</td></tr></table>";
-
 			}
-
-
 echo "<p>";
 rfs_echo($RFS_SITE_COPYRIGHT);
 echo "</p>";
-
-
 
 rfs_echo($RFS_SITE_BODY_CLOSE);
 rfs_echo($RFS_SITE_HTML_CLOSE);
