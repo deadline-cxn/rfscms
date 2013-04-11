7678277c7ac1e373fa21ad2ffa746a3cf9277bc6
sethcoder <defectiveseth@gmail.com>
2013-04-11 17:15:39 -0400

    gitupdate.sh

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,5 +1,2 @@
-## In order for this to work properly, you must
-## add the following line to /etc/sudoers
-## www-data ALL=(ALL) NOPASSWD: ALL
-sudo git pull https://github.com/sethcoder/rfscms.git
+git pull https://github.com/sethcoder/rfscms.git
 
