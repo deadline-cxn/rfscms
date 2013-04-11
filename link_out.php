737cf956e57d2a04edf27344dbdd024654e52a63
sethcoder <defectiveseth@gmail.com>
2013-04-11 12:11:45 -0400

    link_out.php update

--- link_out.php
+++ link_out.php
@@ -4,12 +4,15 @@ include("config/config.php");
 include("include/lib.mysql.php");
 include("include/lib.domain.php");
 $link_out=$_REQUEST['link'];
-echo $link_out;
+$foundlink=false;
 $result=sc_query("select * from link_bin where `link` like '%$link_out%'");
 if(mysql_num_rows($result)>0) {
+	$foundlink=true;
 	$link=mysql_fetch_object($result); $link->clicks=$link->clicks+1;
 	sc_query("update link_bin set `clicks` = '$link->clicks' where `id` = '$link->id'");
 }
 if(empty($link_out)) $link_out=$site_url;
+sc_query("insert into link_bin 		(`name`,`link`,`sname`,`clicks`)
+								VALUES ('$link_out','http://$link_out','$link_out','1'); ");
 echo "<META HTTP-EQUIV=\"refresh\" content=\"0;URL=http://$link_out\">";
 ?>
