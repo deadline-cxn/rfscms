3e61fdbdac3a5f1052f0a6bdc662048db3d3a81e
sethcoder <defectiveseth@gmail.com>
2013-04-12 09:30:23 -0400

    add push method

---
+++ push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ver=$(grep 'v' include/version.php | sed 's/[^0-9.]//g')
+va=$(<build.dat);
+va=`expr $va + 1`;
+echo $va > build.dat;
+echo "RFS CMS $ver BUILD $va";
+git commit -a
+git push github
+
