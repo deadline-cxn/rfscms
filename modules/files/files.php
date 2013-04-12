8e22a2318d0ae3fba3979df2719e7d9d33d8dea0
sethcoder <defectiveseth@gmail.com>
2013-04-12 00:26:54 -0400

    files.php update
    Former-commit-id: 6dfaff4df929a19911e8537c355c5bdc539a9662

--- modules/files/files.php
+++ modules/files/files.php
@@ -770,8 +770,8 @@ if( ($action=="listcategory") ||  ($action=="search") ) {
 	}
 	
 	if($prevtop>0) 
-		echo "<a href=\"$RFS_SITE_URL/modules/files/files.php?action=listcategory&amount=$amount&top=$prevtop&category=$category\">PREV PAGE</a>";
-	echo "<a href=\"$RFS_SITE_URL/modules/files/files.php?action=listcategory&amount=$amount&top=$nexttop&category=$category\">NEXT PAGE</a>";
+		echo "[<a href=\"$RFS_SITE_URL/modules/files/files.php?action=listcategory&amount=$amount&top=$prevtop&category=$category&criteria=$criteria\">PREV PAGE</a>] ";
+	echo " [<a href=\"$RFS_SITE_URL/modules/files/files.php?action=listcategory&amount=$amount&top=$nexttop&category=$category&criteria=$criteria\">NEXT PAGE</a>] ";
 	
     if(count($filelist)) {
 		echo "<h1>".ucwords($buffer)."</h1>";
