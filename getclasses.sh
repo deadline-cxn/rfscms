f0ecfcf80b1a6ff32154bcd333d94b8432f1dbf5
sethcoder <defectiveseth@gmail.com>
2013-04-12 22:33:27 -0400

    updates

--- getclasses.sh
+++ getclasses.sh
@@ -1,2 +1,10 @@
-grep -R --exclude-dir=3rdparty --exclude-dir=files --exclude-dir=images --exclude-dir=images_not_included --exclude-dir=facebook --exclude-dir=backup  'class=' * > classes.txt
+grep -R \
+--exclude-dir=3rdparty \
+--exclude-dir=files \
+--exclude-dir=images \
+--exclude-dir=images_not_included \
+--exclude-dir=facebook \
+--exclude-dir=backup \
+--exclude-dir=modules/netman \
+'class=' * > classes.txt
 
