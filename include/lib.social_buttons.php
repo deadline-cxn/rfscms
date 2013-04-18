19f775ce122d130b898365268a541e3a82552798
sethcoder <defectiveseth@gmail.com>
2013-04-18 10:12:58 -0400

    update facebook comments

--- include/lib.social_buttons.php
+++ include/lib.social_buttons.php
@@ -380,7 +380,7 @@ function sc_facebook_comments($page) {
 	data-url=\"$page\"
 	data-title=\"$page\"
 	data-href=\"$page\"
-	data-width=\"470\"
+	data-width=\"100%\"
 	data-num-posts=\"10\"></div>";
 	}
 }
