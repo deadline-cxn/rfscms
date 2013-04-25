46e33b91ca092ea23738946991e851311d9fbbcd
sethcoder <defectiveseth@gmail.com>
2013-04-25 00:13:37 -0400

    update themes

--- modules/pictures/lib.pictures.php
+++ modules/pictures/lib.pictures.php
@@ -20,16 +20,21 @@ function sc_module_mini_pictures($x) { eval(scg());
         $picture=mysql_fetch_object($res2);
         if($picture->sfw=="no") $picture->url="$RFS_SITE_URL/files/pictures/NSFW.gif";        
         echo "<tr><td class=contenttd>";
-        echo "<a href=\"$RFS_SITE_URL/modules/pictures/pics.php?action=view&id=$picture->id\">".sc_picthumb("$RFS_SITE_PATH/$picture->url",50,0,1)."</a>";        
+        echo "<a href=\"$RFS_SITE_URL/modules/pictures/pics.php?action=view&id=$picture->id\">".
+		        sc_picthumb("$RFS_SITE_PATH/$picture->url",50,0,1)."</a>";
         echo "</td><td class=contenttd width='95%' valign=top>";
         echo "<a href=\"$RFS_SITE_URL/modules/pictures/pics.php?action=view&id=$picture->id\">";
         echo "$picture->sname</a><br>";
         echo sc_trunc($picture->description,50);        
         echo "</td></tr>";
     }
-	echo "</table>";
-    echo "<p align=right>(<a href=$RFS_SITE_URL/modules/pictures/pics.php?action=random class=a_cat>Random Picture</a>)";
+	echo "<tr><td></td><td>";
+echo "<p align=right>(<a href=$RFS_SITE_URL/modules/pictures/pics.php?action=random class=a_cat>Random Picture</a>)";
     echo "(<a href=$RFS_SITE_URL/modules/pictures/pics.php class=a_cat>More...</a>)</p>";
+	echo "</td></tr>";
+	
+	echo "</table>";
+    
 }
 
 function sc_mini_meme($inmid) { eval(scg());
