06656ee993d0574f94c0bf537afe7ed0c525d42e
sethcoder <defectiveseth@gmail.com>
2013-04-25 00:26:54 -0400

    update themes

--- footer.php
+++ footer.php
@@ -18,24 +18,23 @@ if(file_exists($tf)){
 else{
     echo "<BR><BR><BR>";
 	
-	if(empty($data->donated)){
+	
 		
-		if(empty($data->donated)) {
-			sc_google_adsense($RFS_SITE_GOOGLE_ADSENSE);
-		}
-		else {
-			
-		}
-		
-		echo "</td>";
-		
-		
-		echo "<td class=\"righttd\" valign=top>";
-		
-			sc_draw_module("right");
-		
-		echo "</td></tr></table>";
+	if(empty($data->donated)) {
+		sc_google_adsense($RFS_SITE_GOOGLE_ADSENSE);
 	}
+	else {
+		
+	}
+	
+	echo "</td>";
+		
+	echo "<td class=\"righttd\" valign=top>";
+	
+		sc_draw_module("right");
+	
+	echo "</td></tr></table>";
+
     echo "<center>$RFS_SITE_COPYRIGHT</center>";
 	for($i=0;$i<10;$i++) echo "<p> &nbsp; </p> <br> &nbsp; <br>";
     echo "</body></html>";
