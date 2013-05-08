1446215fa1092beb56532d3a379ccb0cbc383c64
sethcoder <defectiveseth@gmail.com>
2013-05-07 23:04:12 -0400

    update warnings
    Former-commit-id: 61d1754252a9c8efe475bf111a5c2c21e9be516b

--- include/lib.rfs.php
+++ include/lib.rfs.php
@@ -12,10 +12,10 @@ function sc_var($x) {
 	echo "\$$x=[".$GLOBALS[$x]."]<br>";
 }
 function sc_system_message() { eval(scg());
-	if(empty($data->pass)) {
+	if($_SESSION['logged_in'])
+		if(empty($data->pass)) {
 		sc_info("You have not established a password. [<a href=\"$RFS_SITE_URL/modules/profile/profile.php?act=show_password_form\" style=\"font-size: 1em;\">Change your password</a>]","WHITE","RED");
-		
-		}
+	}
 	
 	
 }
