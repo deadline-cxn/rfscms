b145a001b6597e46ac1b041511686f4063aaec71
sethcoder <defectiveseth@gmail.com>
2013-05-10 13:33:05 -0400

    update php theme

---
+++ themes/php/t.footer.php
@@ -0,0 +1,43 @@
+
+ </div>
+ <div class="cleaner">&nbsp;</div>
+</div>
+
+<div id="footnav">
+ <a href="http://www.php.net/source.php?url=/releases/5_4_0.php">show source</a> |
+ <a href="http://www.php.net/credits.php">credits</a> |
+ <a href="http://www.php.net/stats/">stats</a> |
+ <a href="http://www.php.net/sitemap.php">sitemap</a> |
+ <a href="http://www.php.net/contact.php">contact</a> |
+ <a href="http://www.php.net/contact.php#ads">advertising</a> |
+ <a href="http://www.php.net/mirrors.php">mirror sites</a>
+</div>
+
+<div id="pagefooter">
+ <div id="copyright">
+ <? echo "$RFS_SITE_COPYRIGHT";
+  // <a href="http://www.php.net/copyright.php">Copyright © 2001-2013 The PHP Group</a><br>
+  // All rights reserved.
+  ?>
+ </div>
+
+ <div id="thismirror">
+  <?
+  /*<a href="http://www.php.net/mirror.php">This mirror</a> generously provided by:
+  <a href="http://developer.yahoo.com/">Yahoo! Inc.</a><br>
+  Last updated: Thu May  9 04:41:09 2013 PDT
+  */
+  ?>
+ </div>
+</div>
+<!--[if IE 6]>
+<script type="text/javascript">
+    /*Load jQuery if not already loaded*/ if(typeof jQuery == 'undefined'){ document.write("<script type=\"text/javascript\"   src=\"//ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js\"></"+"script>"); var __noconflict = true; }
+    var IE6UPDATE_OPTIONS = {
+        icons_path: "/ie6update/images/"
+    }
+</script>
+<script type="text/javascript" src="/ie6update/ie6update.js"></script>
+<![endif]-->
+
+</body></html>
