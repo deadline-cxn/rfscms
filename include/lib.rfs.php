00e2e5cad6843175cf41860744dd341d3b70450e
sethcoder <defectiveseth@gmail.com>
2013-06-05 13:53:02 -0400

    update gns3
    Former-commit-id: 34256ed43617e2d7367dc71f2195dd504561622a

--- include/lib.rfs.php
+++ include/lib.rfs.php
@@ -7,7 +7,20 @@
 srand((double) microtime() * 1000000);  // randomize timer
 setlocale(LC_MONETARY, $RFS_SITE_LOCALE);
 /////////////////////////////////////////////////////////////////////////
-
+function sc_hextorgb($hex) {
+	$hex=str_replace("#","",$hex);
+	if(strlen($hex) == 3) {
+      $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+      $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+      $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+   } else {
+      $r = hexdec(substr($hex,0,2));
+      $g = hexdec(substr($hex,2,2));
+      $b = hexdec(substr($hex,4,2));
+   }
+   $rgb = array($r, $g, $b);
+   return $rgb; // returns an array with the rgb values	
+}
 function sc_generate_long_uid($y) {
 	global $data;
 	$x=time().".".md5($y.$data->name.generate_password());
