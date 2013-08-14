6e300814b5f5c77f57de6f6b17fc7b8661f182ad
sethcoder <defectiveseth@gmail.com>
2013-08-14 09:32:32 -0400

    update tools

--- tools/getrfsfuncs.sh
+++ tools/getrfsfuncs.sh
@@ -1,16 +1,15 @@
-grep -nR \
---exclude-dir=develop \
+grep -Rn \
 --exclude-dir=log \
 --exclude-dir=wtf \
 --exclude-dir=3rdparty \
 --exclude-dir=files \
---exclude-dir=files_h \
---exclude-dir=files_b \
 --exclude-dir=images \
 --exclude-dir=images_not_included \
 --exclude-dir=facebook \
 --exclude-dir=backup \
+--exclude-dir=log \
 --exclude-dir=tools \
 --exclude-dir=modules/netman \
---exclude-dir=not_included \
-"$1" ../*
+'$RFS' ../*
+
+
