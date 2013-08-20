fefe14c4279790382be6711457ccf8de0ac4373b
sethcoder <defectiveseth@gmail.com>
2013-08-20 19:26:44 -0400

    update version 3.2.0 (beta)
    Former-commit-id: 87174e2a50e72c09bfa3d9d09b26bf91d2a4dff0

--- include/version.php
+++ include/version.php
@@ -1,9 +1,6 @@
 <? $RFS_VERSION="v3.2.0 beta";
 $RFS_BUILD="001";
 $file=fopen("build.dat","r");
-if($file) {
-$RFS_BUILD=fgets($file,256);
-fclose($file);
-}
+if($file) { $RFS_BUILD=fgets($file,256); fclose($file); }
 $RFS_FULL_VERSION=$RFS_VERSION." BUILD ".$RFS_BUILD;
 ?>
