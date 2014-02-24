e19f54d6ad5732609a43825e5f1c1619dfc05646
Seth Parson <defectiveseth@gmail.com>
2014-02-24 09:26:59 -0500

    update database upgrade
    Former-commit-id: 333636bfd690ac406feb417baa35d0ba4579fa53

--- install/database_upgrades.php
+++ install/database_upgrades.php
@@ -9,6 +9,9 @@ sc_query("ALTER TABLE `menu_top` ADD `access_method` TEXT");
 sc_query("ALTER TABLE `menu_top` ADD `other_requirements` TEXT");
 sc_query("ALTER TABLE `menu_top` DROP `access`");
 
+sc_query("update `menu_top` set `access_method`='admin,access' where `name`='Admin'");
+sc_query("update `menu_top` set `other_requirements`='loggedin=true' where `name`='Profile'");
+
 
 
 ?>
