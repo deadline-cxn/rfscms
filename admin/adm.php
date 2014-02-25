05d5ff9feeb0dc53f4b86ad3e8e0d33b0baf3073
sethcoder <defectiveseth@gmail.com>
2014-02-24 20:40:24 -0500

    update small update

--- admin/adm.php
+++ admin/adm.php
@@ -376,19 +376,20 @@ function adm_action_form_builder() { eval(scg());
 */
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // ADM_NEW PAGE FUNCTIONS
+/*
 function adm_action_new_page() { eval(scg());
 	echo"<p>Create a new page.</p>";
 	sc_bqf( "action=new_page_go".$RFS_SITE_DELIMITER."SHOW_TEXT_name=name.php","Create new page");
 	include( "footer.php" );
 	exit();
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////
 function adm_action_new_page_go() { eval(scg());
 	if(!file_exists($_GLOBALS['name'])){
 	copy("_template.php",$_GLOBALS['name']);
 	}
 	echo "<p> New file ".$_GLOBALS['name']." created. </p>";
 }
+*/
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // ADM_EMAIL
 function adm_action_email() { eval(scg());
@@ -1828,6 +1829,7 @@ function adm_action_user_edit() {
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // ADM_RSS EDITOR
+/*
 function adm_action_f_rss_edit_go_edit() {
 	eval( scg() );
 	if( $update=="update" ) sc_query( "UPDATE rss_feeds SET `feed`='$edfeed' where `id`='$oid'" );
@@ -1865,6 +1867,7 @@ function adm_action_rss_edit() {
 	include("footer.php");
 	exit();
 }
+*/
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // ADM_SMILEY EDITOR
 function adm_action_edit_smilies() {
@@ -2044,161 +2047,7 @@ function adm_action_f_edit_award_go() {
 	adm_action_awards_edit();
 	
 }
-///////////////////////////////////////////////////////////////////////////////////////////////
-// ADM_LINK EDIT
-function adm_action_f_add_link() {
-	eval( scg() );
-	$link=$_REQUEST['link'];
-	$time=date( "Y-m-d H:i:s" );
-	if( $data->id==0 ) $data->id=999;
-	sc_query( "insert into `link_bin` (`link`,`sname`,`time`,`bumptime`,`poster`,`description`)
-	          values('$link','$sname','$time','$time',   '$id','$description')" );
-	echo "<p>Link [$link][$sname] added to linkbin...</p>\n";
-	sc_log( "*****> $data->name added a link to the linkbin [$link]" );
-	adm_action_edit_linkbin();
-}
-function adm_action_f_modify_link() {
-	eval( scg() );
-	if( $deletelink=="delete" ) {
-		$l=mfo1( "select * from link_bin where `id`='$linkid'" );
-		sc_confirmform( "Are you sure you want to delete $l->link ?",
-                        "$RFS_SITE_URL/admin/adm.php",
-                        "action=f_modify_link".$RFS_SITE_DELIMITER."deletelink=delete_go".$RFS_SITE_DELIMITER."linkid=$linkid" );
-	}
-	if( $deletelink=="delete_go" ) {
 
-		$l=mfo1( "select * from link_bin where `id`='$linkid'" );
-		sc_query( "DELETE FROM link_bin where `id` = '$linkid' limit 1", $mysql );
-		sc_log( "*****> $data->name deleted a link from the linkbin $l->short_name $l->link" );
-
-		sc_info( "$l->link deleted from the link bin","white","red" );
-
-		adm_action_edit_linkbin();
-	}
-	if( $renamelink=="modify" ) {
-		echo "<p><h3>Modifying Link!</h3></p>\n";
-		$short_name=addslashes( $short_name );
-		$linkurl=addslashes( $linkurl );
-		$description=addslashes( $description );
-		$category=addslashes( $category );
-		sc_query( "update link_bin set `sname` = '$short_name' where `id` = '$linkid'" );
-		sc_query( "update link_bin set `link` = '$linkurl' where `id` = '$linkid'" );
-		sc_query( "update link_bin set `description` = '$description' where `id` = '$linkid'" );
-		$hide=0;
-		if( $hidden=="yes" ) {
-			$hide=1;
-		}
-		if( $hidden=="no" )  {
-			$hide=0;
-		}
-		sc_query( "update link_bin set `friend` = '$friend' where `id` = '$linkid'");
-		sc_query( "update link_bin set `hidden` = '$hide' where `id` = '$linkid'" );
-		sc_query( "update link_bin set `referral` = '$referral' where `id` = '$linkid'" );
-		sc_query( "update link_bin set `referrals` = '$referrals' where `id` = '$linkid'" );
-		sc_query( "update link_bin set `clicks` = '$clicks' where `id` = '$linkid'" );
-		sc_query( "update link_bin set `category` = '$category' where `id` = '$linkid'" );
-		sc_query( "update link_bin set `rating` = '$rating' where `id` = '$linkid'" );
-		adm_action_edit_linkbin();
-	}
-}
-function adm_action_edit_linkbin() {
-	eval( scg() );
-	echo "<h3>Link Bin Editor</h3>\n";
-	$result=sc_query( "select * from link_bin order by time desc" );
-	$numlinks=mysql_num_rows( $result );
-	echo "<table width=100% border=0 cellspacing=0 cellpadding=4 align=center>\n";
-	$gt=2;
-	for( $i=0; $i<$numlinks; $i++ ) {
-		$gt++;
-		if( $gt>2 )$gt=1;
-		echo "<tr><td class=sc_project_table_$gt><br>\n";
-		$link=mysql_fetch_object( $result );
-		$userdata=sc_getuserdata( $link->poster );
-		echo "<table border=0 cellspacing=0 cellpadding=0 width=100% >\n";
-		echo "<form enctype=\"application/x-www-form-URLencoded\" action=\"$RFS_SITE_URL/admin/adm.php\" method=\"post\">\n";
-		echo "<input type=\"hidden\" name=\"action\" value=\"f_modify_link\">\n";
-		echo "<input type=\"hidden\" name=\"linkid\" value=\"$link->id\">\n";
-
-echo "<tr class=sc_project_table_$gt>\n";
-
-echo "<td class=sc_project_table_$gt>Short Name</td>";
-echo "<td class=sc_project_table_$gt width=230><input type=text name=short_name value=\"$link->sname\" size=28></td>";
-
-echo "<td class=sc_project_table_$gt>URL</td>";
-echo "<td class=sc_project_table_$gt width=250><input type=text name=linkurl value=\"$link->link\" size=40> </td>\n";
-
-echo "<td class=sc_project_table_$gt width=300>(submitted by $userdata->name on ".sc_time( $link->time ).")</td>\n";
-echo "<td class=sc_project_table_$gt>Rating:</td>\n";
-echo "<td class=sc_project_table_$gt width=100 align=center><input type=submit name=renamelink value=modify></td>\n";
-echo "</tr>\n";
-
-echo "<tr>\n";
-
-echo "<td class=sc_project_table_$gt>Category</td>";
-
-		echo "<td class=sc_project_table_$gt>\n";
-		echo "<select name=category>\n";
-		echo "<option>$link->category\n";
-
-		$result2=sc_query( "select * from `categories` order by name asc" );
-		$numcats=mysql_num_rows( $result2 );
-		for( $i2=0; $i2<$numcats; $i2++ ) {
-			$cat=mysql_fetch_object( $result2 );
-			echo "<option>$cat->name\n";
-		}
-
-		echo "</select>\n";
-		echo "</td>\n";
-
-	echo "<td class=sc_project_table_$gt>Description</td>";
-		echo "<td class=sc_project_table_$gt><input type=text name=description value=\"$link->description\" size=40></td>\n";
-		echo "<td class=sc_project_table_$gt><table border=0><tr>\n";
-		echo "<td class=sc_project_table_$gt>referrals</td><td class=sc_project_table_$gt><input type=text size=4 name=referrals value=\"$link->referrals\"></td>\n";
-		echo "<td class=sc_project_table_$gt>clicks</td><td class=sc_project_table_$gt><input type=text size=4 name=clicks value=\"$link->clicks\"></td>\n";
-		
-		
-		echo "<td class=sc_project_table_$gt>";
-		
-		
-		if( sc_yes($link->hidden) ) echo "hidden <select name=hidden><option>yes<option>no</select>\n";
-		else echo "hidden <select name=hidden><option>no<option>yes</select>\n";
-		echo "<br>";
-				
-		if( sc_yes($link->friend) ) echo "friend <select name=friend><option>yes<option>no</select>\n";
-		else echo "friend <select name=friend><option>no<option>yes</select>\n";		
-		echo "<br>";
-		
-		if( sc_yes($link->referral) ) echo "referral <select name=referral><option>yes<option>no</select>\n";
-		else echo "referral <select name=referral><option>no<option>yes</select>\n";		
-		echo "<br>";
-		
-		echo "</tr></table></td>\n";
-		echo "<td class=sc_project_table_$gt><select name=rating><option>$link->rating\n";
-		for( $j=1; $j<6; $j++ ) echo "<option>$j\n";
-		echo "</select></td>\n";
-		echo "<td class=sc_project_table_$gt align=center><input type=submit name=deletelink value=delete></td>\n";
-
-		echo "</tr>\n";
-		echo "<tr><td class=sc_project_table_$gt>&nbsp;</td><td class=sc_project_table_$gt>&nbsp;</td><td class=sc_project_table_$gt>&nbsp;</td><td class=sc_project_table_$gt>&nbsp;</td><td class=sc_project_table_$gt>&nbsp;</td></tr>\n";
-		echo "</table>\n";
-		echo "</form>\n";
-		echo "</td></tr>\n";
-	}
-
-	echo "<tr><td>\n";
-	echo "</td></tr></table>\n";
-	// add a new link here...
-
-	echo "<h2>Add Link</h2>\n";
-
-	sc_bf(  "$RFS_SITE_URL/admin/adm.php", "action=f_add_link",
-            "link_bin", "", "id",
-            "sname".$RFS_SITE_DELIMITER."link".$RFS_SITE_DELIMITER."description",
-            "include", "category",
-            20, "add link" );
-	include("footer.php");
-	exit();
-}
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // ADM_ACTION_EDIT_TAGS
 function adm_action_edit_tags() { eval(scg()); 
@@ -2357,8 +2206,6 @@ function adm_action_() { eval(scg());
 
 	echo "</td></tr></table>";
 
-    //echo "<hr>";
-
     admin_menu_built_in();
 
 	echo "</div>";
@@ -2409,6 +2256,7 @@ function admin_menu_built_in() { eval(scg());
         }
     }
 
+	echo "<div style='clear: left; '>&nbsp;</div>";	
 
 	$cres=sc_query( "select * from categories order by name asc" );
 	$ccount=mysql_num_rows( $cres );
@@ -2459,14 +2307,13 @@ function admin_menu_built_in() { eval(scg());
                 echo "</div>";
             }
         }
-
     }
 
+	echo "<div style='clear: left; '>&nbsp;</div>";	
 
     $mods=sc_get_modules_array() ;
-
+	
     foreach($mods as $mk=>$mv ) {
-
         $func_count=0;
         foreach( $arr['user'] as $k=>$v ) {
             if( stristr( $v,"_lib_$mv") ) {
@@ -2476,48 +2323,39 @@ function admin_menu_built_in() { eval(scg());
         }
 
         if($func_count) {
-
             foreach( $arr['user'] as $k=>$v ) {
                 if( stristr( $v,"_lib_$mv") ) {
                     $x=str_replace( "adm_action_","",$v );
+						if( !empty( $x ) ) {
+							echo "<div style='	float:left;
+													border: 1px solid #000000;
+													margin: 5px;
+													padding:5px 10px;
+													background:#353575;
+													border-radius:12px; ' > ";
+													
+							echo "<a href=\"$RFS_SITE_URL/admin/adm.php?action=$x\">";
 
-                            if( !empty( $x ) ) {
+							$px=str_replace("lib_" ,""   ,$x);
+							$px=str_replace("$mv"."_" ,""   ,$px);
+							$px=str_replace("_"    ," "  ,$px);
+							$px=ucwords($px);
+							$fn=strtolower($px);
+							$fn=str_replace(" ","_",$fn);
+							$img="$RFS_SITE_PATH/modules/$mv/images/$fn.png";
 
-									echo "<div style='	float:left;
-															border: 1px solid #000000;
-															margin: 5px;
-															padding:5px 10px;
-															background:#353575;
-															border-radius:12px; ' > ";
-															
-									echo "<a href=\"$RFS_SITE_URL/admin/adm.php?action=$x\">";
-
-									$px=str_replace("lib_" ,""   ,$x);
-									$px=str_replace("$mv"."_" ,""   ,$px);
-									$px=str_replace("_"    ," "  ,$px);
-									$px=ucwords($px);
-									$fn=strtolower($px);
-									$fn=str_replace(" ","_",$fn);
-									$img="$RFS_SITE_PATH/modules/$mv/images/$fn.png";
-
-								
-									$png="<img src=\"$RFS_SITE_URL/include/button.php?im=$img&t=$px&w=64&h=64&y=20\" border='0' ></a> ";
-									if( !file_exists( $img ) ) {
-										$png="<img src=\"$RFS_SITE_URL/include/button.php?im=$RFS_SITE_PATH/images/icons/exclamation.png&t=$px&w=64&h=64&y=20\" border='0' alt='$fn - $img' text='$fn - $img'></a> ";
-
-								
-								}
-
-								echo $png;		
-                            echo "</a><br>";
-							
-                            echo "<a style='color: #cFcF00;' href='$RFS_SITE_URL/admin/adm.php?action=$x'>$px</a>";
-
-                            echo "</div>";
-                        }
-                    }
-                }
-                
+						
+							$png="<img src=\"$RFS_SITE_URL/include/button.php?im=$img&t=$px&w=64&h=64&y=20\" border='0' ></a> ";
+							if( !file_exists( $img ) ) {
+								$png="<img src=\"$RFS_SITE_URL/include/button.php?im=$RFS_SITE_PATH/images/icons/exclamation.png&t=$px&w=64&h=64&y=20\" border='0' alt='$fn - $img' text='$fn - $img'></a> ";
+						}
+						echo $png;
+						echo "</a><br>";					
+						echo "<a style='color: #cFcF00;' href='$RFS_SITE_URL/admin/adm.php?action=$x'>$px</a>";
+						echo "</div>";
+					}
+				}
+			}                
         }
     }
 	
