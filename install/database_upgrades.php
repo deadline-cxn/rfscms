3739c0c47d5779fe754666201a29d9f35408a454
sethcoder <defectiveseth@gmail.com>
2014-02-24 20:40:24 -0500

    update small update
    Former-commit-id: 05d5ff9feeb0dc53f4b86ad3e8e0d33b0baf3073

--- install/database_upgrades.php
+++ install/database_upgrades.php
@@ -1,24 +1,21 @@
 <?
 
 // TODO: Add database build tracking 
+// sc_database_add("rfs_system","var","text","NOT NULL");
 
 // interim database changes
 
 sc_query("ALTER TABLE `site_vars` ADD `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST");
 sc_query("ALTER TABLE `site_vars` ADD `desc` TEXT");
 sc_query("ALTER TABLE `site_vars` ADD `type` TEXT");
-
 sc_query("ALTER TABLE `menu_top` ADD `access_method` TEXT");
 sc_query("ALTER TABLE `menu_top` ADD `other_requirement` TEXT");
 sc_query("ALTER TABLE `menu_top` DROP `access`");
 sc_query("ALTER TABLE `menu_top` DROP `other_requirements`");
-
 sc_query("update `menu_top` set `access_method`='admin,access' where `name`='Admin'");
 sc_query("update `menu_top` set `other_requirement`='loggedin=true' where `name`='Profile'");
-
 sc_database_add("categories","worksafe", "text", "NOT NULL");
 sc_database_data_add("categories","name","unsorted",0);
-
 // MD5 hash
 sc_database_add("files","md5", "text", "NOT NULL");
 sc_database_add("files","tags","text", "NOT NULL");
@@ -29,8 +26,6 @@ sc_database_add("file_duplicates", "size1", "text", "NOT NULL");
 sc_database_add("file_duplicates", "loc2", "text", "NOT NULL");
 sc_database_add("file_duplicates", "size2", "text", "NOT NULL");
 sc_database_add("file_duplicates", "md5", "text", "NOT NULL");
-
-
 sc_database_add("wiki","name",   		"text","NOT NULL");
 sc_database_add("wiki","revision",		"int",	"NOT NULL");
 sc_database_add("wiki","revised_by",	"text","NOT NULL");
@@ -39,12 +34,8 @@ sc_database_add("wiki","author", 		"text","NOT NULL");
 sc_database_add("wiki","text",   		"text","NOT NULL");
 sc_database_add("wiki","tags",   		"text","NOT NULL");
 sc_database_add("wiki","updated",		"timestamp","ON UPDATE CURRENT_TIMESTAMP NOT NULL");
-
 sc_touch_dir("$RFS_SITE_PATH/images/wiki");
-
-
 sc_touch_dir("$RFS_SITE_PATH/images/news");
-
 sc_database_add("news","name",		"text",	"NOT NULL");
 sc_database_add("news","headline",	"text",	"NOT NULL");
 sc_database_add("news","message",	"text",	"NOT NULL");
--- modules/linkbin/lib.linkbin.php
+++ modules/linkbin/lib.linkbin.php
@@ -23,4 +23,161 @@ function sc_module_mini_link_friends($x) { eval(scg());
 		echo "<div style='clear: left;'></div>";
 	}
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// ADM_LINK EDIT
+function adm_action_f_add_link() {
+	eval( scg() );
+	$link=$_REQUEST['link'];
+	$time=date( "Y-m-d H:i:s" );
+	if( $data->id==0 ) $data->id=999;
+	sc_query( "insert into `link_bin` (`link`,`sname`,`time`,`bumptime`,`poster`,`description`)
+	          values('$link','$sname','$time','$time',   '$id','$description')" );
+	echo "<p>Link [$link][$sname] added to linkbin...</p>\n";
+	sc_log( "*****> $data->name added a link to the linkbin [$link]" );
+	adm_action_edit_linkbin();
+}
+function adm_action_f_modify_link() {
+	eval( scg() );
+	if( $deletelink=="delete" ) {
+		$l=mfo1( "select * from link_bin where `id`='$linkid'" );
+		sc_confirmform( "Are you sure you want to delete $l->link ?",
+                        "$RFS_SITE_URL/admin/adm.php",
+                        "action=f_modify_link".$RFS_SITE_DELIMITER."deletelink=delete_go".$RFS_SITE_DELIMITER."linkid=$linkid" );
+	}
+	if( $deletelink=="delete_go" ) {
+
+		$l=mfo1( "select * from link_bin where `id`='$linkid'" );
+		sc_query( "DELETE FROM link_bin where `id` = '$linkid' limit 1", $mysql );
+		sc_log( "*****> $data->name deleted a link from the linkbin $l->short_name $l->link" );
+
+		sc_info( "$l->link deleted from the link bin","white","red" );
+
+		adm_action_edit_linkbin();
+	}
+	if( $renamelink=="modify" ) {
+		echo "<p><h3>Modifying Link!</h3></p>\n";
+		$short_name=addslashes( $short_name );
+		$linkurl=addslashes( $linkurl );
+		$description=addslashes( $description );
+		$category=addslashes( $category );
+		sc_query( "update link_bin set `sname` = '$short_name' where `id` = '$linkid'" );
+		sc_query( "update link_bin set `link` = '$linkurl' where `id` = '$linkid'" );
+		sc_query( "update link_bin set `description` = '$description' where `id` = '$linkid'" );
+		$hide=0;
+		if( $hidden=="yes" ) {
+			$hide=1;
+		}
+		if( $hidden=="no" )  {
+			$hide=0;
+		}
+		sc_query( "update link_bin set `friend` = '$friend' where `id` = '$linkid'");
+		sc_query( "update link_bin set `hidden` = '$hide' where `id` = '$linkid'" );
+		sc_query( "update link_bin set `referral` = '$referral' where `id` = '$linkid'" );
+		sc_query( "update link_bin set `referrals` = '$referrals' where `id` = '$linkid'" );
+		sc_query( "update link_bin set `clicks` = '$clicks' where `id` = '$linkid'" );
+		sc_query( "update link_bin set `category` = '$category' where `id` = '$linkid'" );
+		sc_query( "update link_bin set `rating` = '$rating' where `id` = '$linkid'" );
+		adm_action_edit_linkbin();
+	}
+}
+function adm_action_edit_linkbin() {
+	eval( scg() );
+	echo "<h3>Link Bin Editor</h3>\n";
+	$result=sc_query( "select * from link_bin order by time desc" );
+	$numlinks=mysql_num_rows( $result );
+	echo "<table width=100% border=0 cellspacing=0 cellpadding=4 align=center>\n";
+	$gt=2;
+	for( $i=0; $i<$numlinks; $i++ ) {
+		$gt++;
+		if( $gt>2 )$gt=1;
+		echo "<tr><td class=sc_project_table_$gt><br>\n";
+		$link=mysql_fetch_object( $result );
+		$userdata=sc_getuserdata( $link->poster );
+		echo "<table border=0 cellspacing=0 cellpadding=0 width=100% >\n";
+		echo "<form enctype=\"application/x-www-form-URLencoded\" action=\"$RFS_SITE_URL/admin/adm.php\" method=\"post\">\n";
+		echo "<input type=\"hidden\" name=\"action\" value=\"f_modify_link\">\n";
+		echo "<input type=\"hidden\" name=\"linkid\" value=\"$link->id\">\n";
+
+echo "<tr class=sc_project_table_$gt>\n";
+
+echo "<td class=sc_project_table_$gt>Short Name</td>";
+echo "<td class=sc_project_table_$gt width=230><input type=text name=short_name value=\"$link->sname\" size=28></td>";
+
+echo "<td class=sc_project_table_$gt>URL</td>";
+echo "<td class=sc_project_table_$gt width=250><input type=text name=linkurl value=\"$link->link\" size=40> </td>\n";
+
+echo "<td class=sc_project_table_$gt width=300>(submitted by $userdata->name on ".sc_time( $link->time ).")</td>\n";
+echo "<td class=sc_project_table_$gt>Rating:</td>\n";
+echo "<td class=sc_project_table_$gt width=100 align=center><input type=submit name=renamelink value=modify></td>\n";
+echo "</tr>\n";
+
+echo "<tr>\n";
+
+echo "<td class=sc_project_table_$gt>Category</td>";
+
+		echo "<td class=sc_project_table_$gt>\n";
+		echo "<select name=category>\n";
+		echo "<option>$link->category\n";
+
+		$result2=sc_query( "select * from `categories` order by name asc" );
+		$numcats=mysql_num_rows( $result2 );
+		for( $i2=0; $i2<$numcats; $i2++ ) {
+			$cat=mysql_fetch_object( $result2 );
+			echo "<option>$cat->name\n";
+		}
+
+		echo "</select>\n";
+		echo "</td>\n";
+
+	echo "<td class=sc_project_table_$gt>Description</td>";
+		echo "<td class=sc_project_table_$gt><input type=text name=description value=\"$link->description\" size=40></td>\n";
+		echo "<td class=sc_project_table_$gt><table border=0><tr>\n";
+		echo "<td class=sc_project_table_$gt>referrals</td><td class=sc_project_table_$gt><input type=text size=4 name=referrals value=\"$link->referrals\"></td>\n";
+		echo "<td class=sc_project_table_$gt>clicks</td><td class=sc_project_table_$gt><input type=text size=4 name=clicks value=\"$link->clicks\"></td>\n";
+		
+		
+		echo "<td class=sc_project_table_$gt>";
+		
+		
+		if( sc_yes($link->hidden) ) echo "hidden <select name=hidden><option>yes<option>no</select>\n";
+		else echo "hidden <select name=hidden><option>no<option>yes</select>\n";
+		echo "<br>";
+				
+		if( sc_yes($link->friend) ) echo "friend <select name=friend><option>yes<option>no</select>\n";
+		else echo "friend <select name=friend><option>no<option>yes</select>\n";		
+		echo "<br>";
+		
+		if( sc_yes($link->referral) ) echo "referral <select name=referral><option>yes<option>no</select>\n";
+		else echo "referral <select name=referral><option>no<option>yes</select>\n";		
+		echo "<br>";
+		
+		echo "</tr></table></td>\n";
+		echo "<td class=sc_project_table_$gt><select name=rating><option>$link->rating\n";
+		for( $j=1; $j<6; $j++ ) echo "<option>$j\n";
+		echo "</select></td>\n";
+		echo "<td class=sc_project_table_$gt align=center><input type=submit name=deletelink value=delete></td>\n";
+
+		echo "</tr>\n";
+		echo "<tr><td class=sc_project_table_$gt>&nbsp;</td><td class=sc_project_table_$gt>&nbsp;</td><td class=sc_project_table_$gt>&nbsp;</td><td class=sc_project_table_$gt>&nbsp;</td><td class=sc_project_table_$gt>&nbsp;</td></tr>\n";
+		echo "</table>\n";
+		echo "</form>\n";
+		echo "</td></tr>\n";
+	}
+
+	echo "<tr><td>\n";
+	echo "</td></tr></table>\n";
+	// add a new link here...
+
+	echo "<h2>Add Link</h2>\n";
+
+	sc_bf(  "$RFS_SITE_URL/admin/adm.php", "action=f_add_link",
+            "link_bin", "", "id",
+            "sname".$RFS_SITE_DELIMITER."link".$RFS_SITE_DELIMITER."description",
+            "include", "category",
+            20, "add link" );
+	include("footer.php");
+	exit();
+}
 ?>
