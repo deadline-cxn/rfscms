18d111a6e8c5e77b868081752e530694fffff2d1
sethcoder <defectiveseth@gmail.com>
2014-03-02 01:48:33 -0500

    update

--- modules/todo_list/todo_list.php
+++ modules/todo_list/todo_list.php
@@ -33,10 +33,30 @@ function todo_list_status_icon() { eval(scg());
 	
 }
 
-function todo_list_action_open() { eval(scg());
+function todo_list_action_open_task_go() { eval(scg());
+echo "INSERTING";
+	sc_query("insert into `todo_list_task` (`name`,`list`) values ('$name','$list');");
+	$id=mysql_insert_id();
+	sc_updb("todo_list_task","id","$id","");
+	$id=$list;
+	todo_list_action_view_todo_list();
+}
+
+function todo_list_action_open_task() { eval(scg());
 	$tdl=mfo1("select * from todo_list where id='$tdl'");
 	echo "<h1>$tdl->name</h1>";
 	echo "Open task<br>";
+	
+	sc_bf(	sc_phpself(),
+			"action=open_task_go".$RFS_SITE_DELIMITER."list=$tdl->id",
+			"todo_list_task",
+			"",
+			"",
+			"id",
+			"omit",
+			"",
+			60,
+			"Open" );	
 }
 
 function todo_list_action_search() { eval(scg());
@@ -46,6 +66,33 @@ function todo_list_action_search() { eval(scg());
 	sc_bqf("SHOW_TEXT_Name","Search");
 }
 
+function todo_list_action_edit_task_go() { eval(scg());
+	sc_updb("todo_list_task","id",$id,"");
+	$id=$list;
+	todo_list_action_view_todo_list();
+}
+
+function todo_list_action_edit_task() { eval(scg());
+	
+	$task=mfo1("select * from todo_list_task where id='$task'");
+	echo "<h1>$task->name</h1>";
+	echo "Edit task<br>";
+	
+	sc_bf(	sc_phpself(),
+			"action=edit_task_go".$RFS_SITE_DELIMITER.
+			"id=$task->id".$RFS_SITE_DELIMITER.
+			"list=$task->list",
+			"todo_list_task",
+			"select * from `todo_list_task` where `id`='$task->id'",
+			"",
+			"id",
+			"omit",
+			"",
+			60,
+			"Modify" );
+	
+}
+
 function todo_list_action_view_todo_list() { eval(scg());
 	$r=sc_query("select * from todo_list where id=$id");
 	$tdl=mysql_fetch_object($r);
@@ -53,14 +100,21 @@ function todo_list_action_view_todo_list() { eval(scg());
 	echo "<h1>$tdl->name</h1>";
 	echo "$tdl->description<br>";
 	sc_button("$RFS_SITE_URL/modules/todo_list/todo_list.php?action=search&tdl=$tdl->id","Search");
-	sc_button("$RFS_SITE_URL/modules/todo_list/todo_list.php?action=open&tdl=$tdl->id","Open");
+	sc_button("$RFS_SITE_URL/modules/todo_list/todo_list.php?action=open_task&tdl=$tdl->id","Open");
 	// if(!empty($tdl->assigned_to)) echo "Assigned to: $tdl->assigned_to<br>";
 	// if(!empty($tdl->owner))       echo "Owner: $tdl->owner<br>";
 	echo "<hr>";
 	
-	echo "<style>
+echo "<style>
 	
 .todo_Resolved {
+	margin: 5px;
+	padding: 5px;
+	background-color: #AFA;
+	color: #000;
+}
+
+.todo_Closed {
 	margin: 5px;
 	padding: 5px;
 	background-color: #AFA;
@@ -80,13 +134,19 @@ function todo_list_action_view_todo_list() { eval(scg());
 	background-color: #FAA;
 	color: #000;
 }
+
+.todo_In_Progress {
+	margin: 5px;
+	padding: 5px;
+	background-color: #FFA;
+	color: #000;
+}
 	
 	</style>";
 	
 $r=sc_query("
 select * from `todo_list_task` 
-where (`list`='$tdl->id') and
-	  (`name`='Stuff') ;");
+where (`list`='$tdl->id')  ;");
 	  
 	$n=mysql_num_rows($r);
 
@@ -95,36 +155,17 @@ where (`list`='$tdl->id') and
 		echo "<table border=0 cellpadding=5 cellspacing=0>";
 			echo "<tr>";
 			
-			echo "<th>";
-			echo "</th>";
-			
-			echo "<th>";
-			echo "Status";
-			echo "</th>";
-			
-			echo "<th>";
-			echo "Priority";
-			echo "</th>";
-			
-			echo "<th>";
-			echo "Name";
-			echo "</th>";
-			
-			echo "<th>";
-			echo "Opened";
-			echo "</th>";
-			
-			echo "<th>";
-			echo "Due";
-			echo "</th>";
-			
-			echo "<th>";
-			echo "Step";
-			echo "</th>";
-			
-			echo "<th>";
-			echo "Action";
-			echo "</th>";
+			if(empty($tdl->type)) $tdl->type="Task";
+			echo "<th>$tdl->type # </th>";
+			echo "<th> </th>";
+			echo "<th> </th>";
+			echo "<th>Status</th>";
+			//echo "<th>Priority</th>";
+			echo "<th>Name</th>";
+			echo "<th>Description</th>";
+			echo "<th>Opened</th>";
+			// echo "<th>Due</th>";
+			// echo "<th>Step</th>";
 			
 			echo "</tr>";		
 		
@@ -134,45 +175,30 @@ where (`list`='$tdl->id') and
 			
 			echo "<tr>";
 			if(empty($task->status)) $task->status="Unknown";
+			$task_status=str_replace(" ","_",$task->status);
 			
-			echo "<td class=\"todo_$task->status\">";
-			echo "<img src=\"$RFS_SITE_URL/modules/todo_list/icons/$task->status.png\">	";
+			echo "<td class=\"todo_$task_status\">$task->id</td>";
+			
+			echo "<td class=\"todo_$task_status\">";
+			sc_button("$RFS_SITE_URL/modules/todo_list/todo_list.php?action=edit_task&task=$task->id","Edit");
 			echo "</td>";
 			
-			echo "<td class=\"todo_$task->status\">";
-			//echo "$task->status<br>";
-
-			// if(sc_access_check());
+			echo "<td class=\"todo_$task_status\"><img src=\"$RFS_SITE_URL/modules/todo_list/icons/$task->status.png\"></td>";
 			
+			echo "<td class=\"todo_$task_status\">";
 			sc_ajax("Status","todo_list_task","id",$task->id,"status","30",
 					"select,table,todo_list_status,name,nolabel","admin","access","");
-					
-			
+
 			echo "</td>";
 			
-			echo "<td class=\"todo_$task->status\">";
-			echo "$task->priority";
-			echo "</td>";
+			//echo "<td class=\"todo_$task_status\">$task->priority</td>";
 			
-			echo "<td class=\"todo_$task->status\">";
-			echo "$task->name";
-			echo "</td>";
+			echo "<td class=\"todo_$task_status\"> $task->name </td>";			
+			echo "<td class=\"todo_$task_status\"> $task->description </td>";			
+			echo "<td class=\"todo_$task_status\"> $task->opened </td>";
 			
-			echo "<td class=\"todo_$task->status\">";
-			echo "$task->opened";
-			echo "</td>";
-			
-			echo "<td class=\"todo_$task->status\">";
-			echo "$task->due";
-			echo "</td>";
-			
-			echo "<td class=\"todo_$task->status\">";
-			echo "$task->step";
-			echo "</td>";
-			
-			echo "<td class=\"todo_$task->status\">";
-			echo "$task->action";
-			echo "</td>";
+			// echo "<td class=\"todo_$task_status\"> $task->due </td>";			
+			// echo "<td class=\"todo_$task_status\">$task->step</td>";
 			
 			echo "</tr>";
 		}
