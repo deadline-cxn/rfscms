0e925c6515c51bca94b1967705ca47bac13fef38
sethcoder <defectiveseth@gmail.com>
2014-03-04 01:28:23 -0500

    update videos rewrite & database backup

--- tools/getfuncs.php
+++ tools/getfuncs.php
@@ -1,12 +1,15 @@
 <?
-$f=file_get_contents("functions.txt");
+if(isset($argv[1])) $fn=$argv[1];
+else $fn="functions.txt";
+$f=file_get_contents($fn);
+$f=str_replace("../","",$f);
 $ex1=explode("\n",$f);
 foreach($ex1 as $k => $v) {
 	$ex=explode(":",$v);
 	$ex2=explode("{",$ex[2]);
 	if($ex2[0][0]=="f") {
-		// echo "=======================================================\n";
-		echo "$ex2[0] ";
+		$eff=str_replace("function ","",$ex2[0]);
+		echo "$eff ";
 		echo "($ex[0]($ex[1]))\n";
 	}
 }
