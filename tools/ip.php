381ec4aa63436268bd3842fb0aaa28575e24c347
sethcoder <defectiveseth@gmail.com>
2014-03-06 10:49:30 -0500

    update

--- tools/ip.php
+++ tools/ip.php
@@ -2,5 +2,7 @@
 // foreach ($_SERVER as $a => $b ) { echo "$a [$b]<br>"; }
 $fp=fopen("ip.txt","wt");
 fputs($fp,$_SERVER['REMOTE_ADDR']);
+$t=date('Y-m-d i:s');
+fputs($fp,"\n<br>\nLast updated:$t\n<br>");
 fclose($fp);
 ?>
