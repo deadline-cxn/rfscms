e2fd6470ce23ca78cc3d5f9354324b3b4f1fb9ca
sethcoder <defectiveseth@gmail.com>
2014-03-08 03:02:00 -0500

    update overhaulin

--- include/session.php
+++ include/session.php
@@ -6,10 +6,7 @@ include_once("lib.sitevars.php");
 session_name(str_replace(" ","_",$RFS_SITE_SESSION_ID));
 session_cache_expire(99999);
 session_start();
-
-include_once("lib.div.php");
 include_once("lib.users.php");
-lib_div(__FILE__);
 if(isset($_SESSION["logged_in"])) $logged_in=$_SESSION["logged_in"];
 if(isset($_SESSION["valid_user"])) $RFS_SITE_SESSION_USER = sc_getusername($_SESSION["valid_user"]);
 if(isset($_REQUEST['admin_show_top'])) $_SESSION['admin_show_top']=$_REQUEST['admin_show_top'];
