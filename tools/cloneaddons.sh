2e99c0e4c645099713a064bb3a73f92788c005e2
sethcoder <defectiveseth@gmail.com>
2014-03-19 12:52:39 -0400

    update stuff

---
+++ tools/cloneaddons.sh
@@ -0,0 +1,9 @@
+cd /var/www
+git clone https://github.com/sethcoder/rfscms_bullet_log.git
+git clone https://github.com/sethcoder/rfscms_gns3.git
+git clone https://github.com/sethcoder/rfscms_netman.git
+git clone https://github.com/sethcoder/rfscms_netman_tools.git
+git clone https://github.com/sethcoder/rfscms-nqt.git
+git clone https://github.com/sethcoder/rfscms_political_game.git
+git clone https://github.com/sethcoder/rfsft.git
+
