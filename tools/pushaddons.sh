e186b537ab86e567dc1491e908324e0d26b93ba3
sethcoder <defectiveseth@gmail.com>
2014-03-19 22:08:56 -0400

    update update

---
+++ tools/pushaddons.sh
@@ -0,0 +1,17 @@
+cd /var/www/modules
+cd rfscms_bullet_log
+gpush 'update'
+cd ../rfscms_gns3
+gpush 'update'
+cd ../rfscms_netman
+gpush 'update'
+cd ../rfscms_netman_tools
+gpush 'update'
+cd ../rfscms-nqt
+gpush 'update'
+cd ../rfscms_political_game
+gpush 'update'
+cd ../rfsft
+gpush 'update'
+
+
