a150b0e25422a6809067c83ebeba563f6ca130f9
sethcoder <defectiveseth@gmail.com>
2014-03-24 18:32:51 -0400

    update update

--- tools/pulladdons.sh
+++ tools/pulladdons.sh
@@ -1,17 +1,24 @@
 cd ../modules
 cd rfscms_bullet_log
+git stash
 git pull https://github.com/sethcoder/rfscms_bullet_log.git
 cd ../rfscms_gns3
+git stash
 git pull https://github.com/sethcoder/rfscms_gns3.git
 cd ../rfscms_netman
+git stash
 git pull https://github.com/sethcoder/rfscms_netman.git
 cd ../rfscms_netman_tools
+git stash
 git pull https://github.com/sethcoder/rfscms_netman_tools.git
 cd ../rfscms-nqt
+git stash
 git pull https://github.com/sethcoder/rfscms-nqt.git
 cd ../rfscms_political_game
+git stash
 git pull https://github.com/sethcoder/rfscms_political_game.git
 cd ../rfsft
+git stash
 git pull https://github.com/sethcoder/rfsft.git
 
 
