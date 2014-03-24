ff063f9cb949f55f349551dcb22b820c57931919
sethcoder <defectiveseth@gmail.com>
2014-03-24 11:58:29 -0400

    update update

--- tools/pushaddons.sh
+++ tools/pushaddons.sh
@@ -1,17 +1,25 @@
 cd /var/www/modules
+echo "PUSHING BULLETLOG\n"
 cd rfscms_bullet_log
 gpush 'update'
+echo "PUSHING GNS3\n"
 cd ../rfscms_gns3
 gpush 'update'
+echo "PUSHING NETMAN\n"
 cd ../rfscms_netman
 gpush 'update'
+echo "PUSHING NETMAN TOOLS\n"
 cd ../rfscms_netman_tools
 gpush 'update'
+echo "PUSHING NQT\n"
 cd ../rfscms-nqt
 gpush 'update'
+echo "PUSHING PG\n"
 cd ../rfscms_political_game
 gpush 'update'
+echo "PUSHING FT\n"
 cd ../rfsft
 gpush 'update'
+
 
 
