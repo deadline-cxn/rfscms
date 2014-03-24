c5e0f9c5bb08d337590e5f2972bb712199f80259
sethcoder <defectiveseth@gmail.com>
2014-03-24 18:31:44 -0400

    update update

--- tools/pulladdons.sh
+++ tools/pulladdons.sh
@@ -1,9 +1,17 @@
-cd /var/www/modules
+cd ../modules
+cd rfscms_bullet_log
 git pull https://github.com/sethcoder/rfscms_bullet_log.git
+cd ../rfscms_gns3
 git pull https://github.com/sethcoder/rfscms_gns3.git
+cd ../rfscms_netman
 git pull https://github.com/sethcoder/rfscms_netman.git
+cd ../rfscms_netman_tools
 git pull https://github.com/sethcoder/rfscms_netman_tools.git
+cd ../rfscms-nqt
 git pull https://github.com/sethcoder/rfscms-nqt.git
+cd ../rfscms_political_game
 git pull https://github.com/sethcoder/rfscms_political_game.git
+cd ../rfsft
 git pull https://github.com/sethcoder/rfsft.git
+
 
