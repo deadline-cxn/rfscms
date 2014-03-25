404a6648edb27e148225021510d458cea2e4e54c
sethcoder <defectiveseth@gmail.com>
2014-03-25 19:18:56 -0400

    update update

--- modules/core_videos/videos.php
+++ modules/core_videos/videos.php
@@ -109,9 +109,9 @@ function videos_action_submitvid_youtube_go() {
 		$cont=$data->id;
 		$time=date("Y-m-d H:i:s"); 
 		$sname=addslashes($sname);
-		$url=addslashes($url);
+		$youtube=addslashes($youtube);
         $q=" INSERT INTO `videos` (`contributor`, `sname`, `embed_code`,  `url`, `time`, `bumptime`, `category`, `hidden`, `sfw`)
-								 VALUES ('$cont',	 	'$sname','$vembed_code' , `$youtube` ,'$time',    '$time','$category',      '0', '$sfw');";
+						 VALUES ('$cont',	 	'$sname','$vembed_code' , `$youtube` ,'$time',    '$time','$category',      '0', '$sfw');";
 		lib_mysql_query($q);
 		$id=mysql_insert_id();
 		videos_action_view($id);
