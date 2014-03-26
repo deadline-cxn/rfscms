90b4668bf7a43b0090fae4e7cefe79e07a0d3d20
sethcoder <defectiveseth@gmail.com>
2014-03-26 08:21:26 -0400

    update fixes

--- include/lib.div.php
+++ include/lib.div.php
@@ -5,7 +5,7 @@
 if(!function_exists("lib_div")) {
 function lib_div($t) {	
 	if(!isset($GLOBALS['RFS_GEN_IMAGE'])) {	
-		d_echo("<!-- ******************** ($t) ******************** -->\n");
+		// d_echo("<!-- ******************** ($t) ******************** -->\n");
 		}
 	}
 }
