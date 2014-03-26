d282a85a3239e18b8c406b8f6f46480e1147ffba
sethcoder <defectiveseth@gmail.com>
2014-03-26 08:24:30 -0400

    update fixes

--- include/lib.div.php
+++ include/lib.div.php
@@ -5,9 +5,34 @@
 if(!function_exists("lib_div")) {
 function lib_div($t) {	
 	if(!isset($GLOBALS['RFS_GEN_IMAGE'])) {	
-		// d_echo("<!-- ******************** ($t) ******************** -->\n");
+		d_echo("<!-- ******************** ($t) ******************** -->\n");
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // this file can not have trailing spaces
 ?>
