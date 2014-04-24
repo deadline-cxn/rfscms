067ca98ec210995e57cc6a3dadc59d57b75367b7
sethcoder <defectiveseth@gmail.com>
2014-04-23 22:32:21 -0400

    update admin

--- admin/adm.php
+++ admin/adm.php
@@ -734,8 +734,11 @@ function adm_action_database_backup() {
 	echo "<h1>Database Backup</h1><hr>";
 	$sn=str_replace("http://","",$RFS_SITE_URL);
 	$sn=str_replace("/","",$sn);
+	
 	echo (lib_mysql_backup_database($RFS_SITE_PATH."/files/.backups/$sn"));
+		
 	echo (lib_mysql_backup_table("addon_database",$RFS_SITE_PATH."/files/addon_database.sql"));
+	
 	echo "</div>";
 	finishadminpage();
 }
