510e648657f3970ad7d89d3e62db6830c6fab844
sethcoder <defectiveseth@gmail.com>
2014-05-27 19:10:43 -0400

    update pull addons script

--- tools/pulladdons.sh
+++ tools/pulladdons.sh
@@ -1,24 +1,7 @@
-cd ../modules
-cd bullet_log
-git stash
-git pull https://github.com/sethcoder/bullet_log.git
-cd ../gns3
-git stash
-git pull https://github.com/sethcoder/gns3.git
-cd ../netman
-git stash
-git pull https://github.com/sethcoder/netman.git
-cd ../netman_tools
-git stash
-git pull https://github.com/sethcoder/netman_tools.git
-cd ../nqt
-git stash
-git pull https://github.com/sethcoder/nqt.git
-cd ../political_game
-git stash
-git pull https://github.com/sethcoder/political_game.git
-cd ../fitness_tracker
-git stash
-git pull https://github.com/sethcoder/fitness_tracker.git
-
-
+./pulladdon.sh bullet_log
+./pulladdon.sh gns3
+./pulladdon.sh netman
+./pulladdon.sh netman_tools
+./pulladdon.sh nqt
+./pulladdon.sh political_game
+./pulladdon.sh fitness_tracker
